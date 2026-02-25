7ad84c1ec1155c7845976cb1118bf80597486e77
MEET0512 <meet.patel.mp13@gmail.com>
2026-02-25 11:44:09 -0700

    project init

---
+++ src/main/java/com/atlaspay/api/AtlaspayApplication.java
@@ -0,0 +1,13 @@
+package com.atlaspay.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtlaspayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtlaspayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atlaspay/api/AtlaspayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atlaspay.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtlaspayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
